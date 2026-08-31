9c8f56c657cf35e6e700d1de080b443363426b1e
Paulina <pauobajdin96@gmail.com>
2026-08-31 18:53:41 +0200

    Initialize Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.safejourney.app',
+  appName: 'SafeJourney',
+  webDir: 'dist'
+};
+
+export default config;
